35ba86c03d16b23e5e2319ebb279d6e6f7a164e3
Joshua Thrower <jthrower1@atu.edu>
2024-01-24 10:12:14 -0600

    Commit 1

--- Practice.cpp
+++ Practice.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "Lets Do This\n";
+    std::cout << "We got This\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
